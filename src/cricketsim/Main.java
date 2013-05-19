c4faf523cdf67d928e41f60eab9b71ace8f250ad
Josh Tumath <josh@joshtumath.me.uk>
2013-05-19 15:53:29 +0200

    Added Javadoc with authors

--- src/cricketsim/Main.java
+++ src/cricketsim/Main.java
@@ -1,5 +1,9 @@
 package cricketsim;
 
+/**
+ * @author Ryan Gouldsmith
+ * @author Josh Tumath (josh@joshtumath.me.uk)
+ */
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
