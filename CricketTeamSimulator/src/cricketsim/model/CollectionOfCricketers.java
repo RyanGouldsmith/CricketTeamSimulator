2c1ce686d2a146c1d43c4e383b361ac92fb2279f
Josh Tumath <josh@joshtumath.me.uk>
2013-05-20 14:07:41 +0100

    Improved naming and added more Javadoc

--- CricketTeamSimulator/src/cricketsim/model/CollectionOfCricketers.java
+++ CricketTeamSimulator/src/cricketsim/model/CollectionOfCricketers.java
@@ -1,34 +1,47 @@
 package cricketsim.model;
 
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * Manages a list of cricketers.
+ * 
 * @author Ryan Gouldsmith (ryangouldsmith@gmail.com)
 * @author Josh Tumath (josh@joshtumath.me.uk)
 */
 public class CollectionOfCricketers {
-	private Vector<Cricketer> cricketers;
+	/** List of cricketers */
+	private List<Cricketer> cricketers = new ArrayList<Cricketer>();
 	
-	public CollectionOfCricketers(){
-		cricketers = new Vector<Cricketer>();
-	}
-	
-	public int collectionOfRuns(){
+	/**
+	 * Get the total runs of all cricketers.
+	 * @return the total runs of all cricketers
+	 */
+	public int getTotalRuns(){
 		int total = 0;
 		
-		for (Cricketer m: cricketers){
-			total += m.getRuns();
+		for (Cricketer cricketer: cricketers){
+			total += cricketer.getRuns();
 		}
 		
 		return total;
 	}
 	
-	public Vector<Cricketer> getList(){
-		return cricketers;
+	/**
+	 * Get a cricketer from a position in the list.
+	 * @param index position in the list
+	 * @return a cricketer
+	 */
+	public Cricketer getCricketer(int index){
+		return cricketers.get(index);
 	}
 	
-	public void addToList(Cricketer a){
-		cricketers.add(a);
+	/**
+	 * Add a cricketer to the list
+	 * @param cricketer cricketer to be added to the list
+	 */
+	public void add(Cricketer cricketer){
+		cricketers.add(cricketer);
 	}
 	
 }
