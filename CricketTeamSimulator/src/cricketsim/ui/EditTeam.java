9cac6851b1a23c75145212f52af4112bc0e91856
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2013-05-20 16:43:52 +0100

    Java Docs added to the EditTeamFrame

--- CricketTeamSimulator/src/cricketsim/ui/EditTeam.java
+++ CricketTeamSimulator/src/cricketsim/ui/EditTeam.java
@@ -1,7 +1,13 @@
 package cricketsim.ui;
 
 import javax.swing.JFrame;
-
+/**
+ * This is where the Team Panel will be added, shows the window where the team can be edited
+ * Editing includes different stats etc.
+ * @author Ryan Gouldsmith (ryangouldsmith@gmail.com)
+ * @author Joshua Tumath (josh@joshtumath.me.uk)
+ *
+ */
 public class EditTeam extends JFrame {
 
 }
