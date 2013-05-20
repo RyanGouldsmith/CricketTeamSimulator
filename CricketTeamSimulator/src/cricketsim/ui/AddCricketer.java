2cdfb0b12529fcf1f70db0facab427f0cee96654
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2013-05-20 16:43:13 +0100

    Java Docs added to the AddCricketerFrame

--- CricketTeamSimulator/src/cricketsim/ui/AddCricketer.java
+++ CricketTeamSimulator/src/cricketsim/ui/AddCricketer.java
@@ -1,7 +1,12 @@
 package cricketsim.ui;
 
 import javax.swing.JFrame;
-
+/**
+ * This class is the main Frame which the user will see for adding a cricketer to the system
+ * @author ryan Gouldmith
+ * @author Joshua Tumath
+ *
+ */
 public class AddCricketer extends JFrame {
 	
 	
