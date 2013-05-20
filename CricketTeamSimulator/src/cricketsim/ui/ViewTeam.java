dcbfab133b595c38ee29200350bbdb2c89f7f458
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2013-05-20 16:44:46 +0100

    Java Docs added to the ViewTeamFrame

--- CricketTeamSimulator/src/cricketsim/ui/ViewTeam.java
+++ CricketTeamSimulator/src/cricketsim/ui/ViewTeam.java
@@ -1,7 +1,13 @@
 package cricketsim.ui;
 
 import javax.swing.JFrame;
-
+/**
+ * This is the Frame where we will see the Team and we can print the team off 
+ * The ViewTeamPanel will be added here
+ * @author Ryan Gouldsmith (ryangouldsmith@gmail.com)
+ * @author Joshua Tumath (josh@joshtumath.me.uk)
+ *
+ */
 public class ViewTeam extends JFrame {
 
 }
