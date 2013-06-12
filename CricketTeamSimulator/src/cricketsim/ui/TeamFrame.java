963b252a2d6a6b09de81aeca11e8e5d76084eb5a
Josh Tumath <josh@joshtumath.me.uk>
2013-06-12 19:37:18 +0100

    Created basic UI for TeamFrame.

--- CricketTeamSimulator/src/cricketsim/ui/TeamFrame.java
+++ CricketTeamSimulator/src/cricketsim/ui/TeamFrame.java
@@ -1,9 +1,14 @@
 package cricketsim.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
 
 import cricketsim.model.Cricketer;
 
@@ -21,9 +26,57 @@ public class TeamFrame extends JFrame {
 	 * @param cricketers a list of all the cricketers in the team
 	 */
 	public TeamFrame(TeamType type, List<Cricketer> cricketers) {
-		setTitle("Best " + type + " Team");
+		String typeString;
+		
+		switch(type) {
+		case ATTACK:
+			typeString = "Attack";
+			break;
+		case DEFENSE:
+			typeString = "Defence";
+			break;
+		case NEUTRAL:
+			typeString = "Neutral";
+			break;
+		default:
+			typeString = null;
+			break;
+		}
+		
+		setTitle("Best " + typeString + " Team");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setMinimumSize(new Dimension(400, 450));
 		
+		setBounds(100, 100, 450, 300);
+		JPanel contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		String[] columnNames = {
+				"Name",
+				"Age",
+				"Position"};
+		JTable table = new JTable(generateTable(cricketers), columnNames);
+		JScrollPane tableScroll = new JScrollPane(table);
+		contentPane.add(tableScroll, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		// TODO: Add labels for showing stats about the team
+		contentPane.add(panel, BorderLayout.EAST);
+		
 		setLocationRelativeTo(null); // Centre window in middle of screen
+	}
+	
+	private String[][] generateTable(List<Cricketer> cricketers) {
+		// TODO
+		
+		// XXX: Test data
+		String[][] foo = {
+			{"Ryan Gouldsmith", "19", "Foo"},
+			{"Josh Tumath", "19", "Bar"}
+		};
+		
+		return foo;
 	}
 }
