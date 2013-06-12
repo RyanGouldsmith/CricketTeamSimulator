cf53ba33a4597be64cd6d1323e6fb15147a4995d
Josh Tumath <josh@joshtumath.me.uk>
2013-06-12 16:45:53 +0100

    Added Javadocs and comments

--- CricketTeamSimulator/src/cricketsim/model/ImportCricketer.java
+++ CricketTeamSimulator/src/cricketsim/model/ImportCricketer.java
@@ -2,6 +2,12 @@ package cricketsim.model;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+
+/**
+ * Imports cricketers from various file types and stores them in the model.
+ * @author Ryan Gouldsmith (ryangouldsmith@gmail.com)
+ * @author Josh Tumath (josh@joshtumath.me.uk)
+ */
 public class ImportCricketer {
 	private BufferedReader cricketReader;
 	private Cricketer cricketer;
@@ -11,12 +17,20 @@ public class ImportCricketer {
 	private int appearances; 
 	private int wickets; 
 	private int totalRuns;
-	public void readCricketers() throws IOException{
+	
+	/**
+	 * Reads cricketers in from a CSV file
+	 * @throws IOException throws if there is a problem whilst reading the file
+	 */
+	public void readCricketers() throws IOException {
+		// XXX: Test input file:
 		cricketReader = new BufferedReader(new FileReader("cricketList.txt"));
 		String userDetails;
+		
 		while((userDetails = cricketReader.readLine()) != null){
 			String[] arrayOfDetails = userDetails.split(",");
 			cricketer = new Cricketer();
+			
 			for (int i=0; i < arrayOfDetails.length; i++){
 				System.out.println();
 				name = arrayOfDetails[0];
@@ -31,8 +45,10 @@ public class ImportCricketer {
 				cricketer.setWickets(wickets);
 				cricketer.setTotalRuns(totalRuns);
 			}
+			
 			System.out.print(cricketer.getName() + " " +  cricketer.getGender() + " " +  cricketer.getPosition() + " " + cricketer.getAppearances() + " " + cricketer.getWickets() + " " + cricketer.getTotalRuns());
 		}
+		
 		cricketReader.close();
 	}
 }			
